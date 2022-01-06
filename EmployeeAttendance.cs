fa512205ec9f8365810880dfb50e4bb3e83e9178
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-01-06 13:12:05 +0530

    Add files via upload

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld1
+{
+    internal class EmployeeAttendance
+    {
+        public static void Getattendace()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+
+        }
+    }
+}
+
